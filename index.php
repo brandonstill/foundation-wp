fdd4ae12331692d30c18c25b6c9aba797d49f27c
Brandon Still <brandon@agencyrevolution.com>
2016-04-20 12:39:06 -0700

    move sidebar

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
   <section role="main" class="main">
     <div class="row">
-      <div class="aside" role="complementary">
-        <?php get_sidebar(); ?>
-      </div>
-      <div class="content">
+      <div class="content columns medium-8">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <?php the_content(); ?>
           <?php endwhile; else: ?>
             <p><?php _e('Nothing here to see.'); ?></p>
         <?php endif; ?>
+      </div>
+      <div class="aside columns medium-4" role="complementary">
+        <?php get_sidebar(); ?>
       </div>
     </div>
   </section>
